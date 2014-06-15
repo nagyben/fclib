40f4118809370f48590863c242a0a6c9718b566c
exantas <nagy.benb@gmail.com>
2014-06-15 13:18:32 +0100

    Added UserData.cs

---
+++ fclib/UserData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace fclib {
+	/// <summary>
+	/// Provides helper functions to save user data
+	/// </summary>
+	public static class UserData {
+		public static void SaveRules(List<Rule> rulelist, string filepath) {
+			Stream stream = File.Open(filepath, FileMode.Create);
+			BinaryFormatter bformatter = new BinaryFormatter();
+			
+			bformatter.Serialize(stream, rulelist);
+			stream.Close();
+		}
+
+		public static List<Rule> LoadRules(string filepath) {
+			List<Rule> rulelist;
+			try {
+				Stream stream = File.Open(filepath, FileMode.Open);
+				BinaryFormatter bformatter = new BinaryFormatter();
+
+				rulelist = (List<Rule>)bformatter.Deserialize(stream);
+			} catch (Exception) {
+				throw;
+			}
+			return rulelist;
+		}
+	}
+}
