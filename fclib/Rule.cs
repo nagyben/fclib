3440833e490a542033619d000c6843102cd5d576
exantas <nagy.benb@gmail.com>
2014-06-15 13:17:32 +0100

    Completed rule serializing

--- fclib/Rule.cs
+++ fclib/Rule.cs
@@ -10,41 +10,50 @@ namespace fclib {
 	[Serializable]
 	public class Rule : ISerializable {
 
+		/* INTRODUCTION
+		 * The Rule class provides the instances which describe each file copying / moving / deleting rule
+		 */
+
 		/* MEMBER VARIABLES */
 		private int _id;
 		public int ID {
 			get { return _id; }
 			set {
 				if (value < 0) {
-					throw new ArgumentOutOfRangeException("Rule.ID","Rules cannot have a negative ID number");
+					throw new ArgumentOutOfRangeException("Rule.ID", "Rules cannot have a negative ID number");
 				} else {
 					this._id = value;
 				}
 			}
 		}
+
 		private string _name;
 		public string Name {
 			get { return _name; }
 			set {
-				if (value == "") {
+				if (value == "" || value == null) {
 					_name = string.Join("|", this.Filters) + "[" + string.Join(",", this.Extensions) + "] from <" + this.ParentDirectory + "> to <" + this.TargetDirectory + ">";
 				}
 			}
 		}
-		public List<string> Extensions { get; private set; }
-		public List<string> Filters { get; private set; }
 
-		private string _ParentDirectory;
+		public List<string> Extensions { get; private set; }	// List of extenstions in the format ".mkv", ".avi" etc.
+		public List<string> Filters { get; private set; }		/* List of string filters, eg. "family guy" or "simpsons"
+																 * these are not case-sensitive as all working filenames
+																 * are converted to lowercase */
+
+		private string _ParentDirectory;						// The directory which files will be moved / copied / deleted from
 		public string ParentDirectory {
-			get { return this._ParentDirectory;  }
+			get { return this._ParentDirectory; }
 			set {
 				// TODO: filepath logic
 				this._ParentDirectory = value;
 			}
 		}
-		private string _TargetDirectory;
-		public string TargetDirectory {
-			get { return this._TargetDirectory; }
+
+		private string _TargetDirectory;						// The directory which files will be moved / copied to
+		public string TargetDirectory {							// note that this does not apply when the rule is a
+			get { return this._TargetDirectory; }				// delete operation
 			set {
 				// TODO: filepath logic
 				this._TargetDirectory = value;
@@ -54,21 +63,25 @@ namespace fclib {
 		public enum Operation {
 			Copy = 0,
 			Move = 1,
-			Delete =2
+			Delete = 2
 		}
-		public Operation Instruction = Operation.Copy;
-		public bool SearchSubdirectories = false;
+		public Operation Instruction = Operation.Copy;			// move, copy or delete
+		public bool SearchSubdirectories = false;				/* Whether to check the subfolders of the parent directory.
+																 * Does not apply to the target directory because this is
+																 * ambiguous.
+																 */
 
 		/* CONSTRUCTORS */
 
 		// basic constructor
-		public Rule(int id,						
+		public Rule(int id,
 					string parentdir,
 					string targetdir,
 					List<string> extensions,
 					List<string> filters,
 					string name = "") {
-						
+
+			// The checks should be done by the set methods as defined by the properties
 			this.ID = id;
 			this.ParentDirectory = parentdir;
 			this.TargetDirectory = targetdir;
@@ -77,7 +90,7 @@ namespace fclib {
 			this.Name = name;
 		}
 
-		// serialization constructor
+		/* SERIALIZATION */
 		public Rule(SerializationInfo info, StreamingContext context) {
 			this.ID = (int)info.GetValue("ID", typeof(int));
 			this.ParentDirectory = (string)info.GetValue("ParentDirectory", typeof(string));
@@ -85,15 +98,54 @@ namespace fclib {
 			this.Extensions = (List<string>)info.GetValue("Extensions", typeof(List<string>));
 			this.Filters = (List<string>)info.GetValue("Filters", typeof(List<string>));
 			this.Name = (string)info.GetValue("Name", typeof(string));
+			this.Instruction = (Operation)info.GetValue("Instruction", typeof(Operation));
+			this.SearchSubdirectories = (bool)info.GetValue("Subdirectories", typeof(bool));
+		}
+
+		public void GetObjectData(SerializationInfo info, StreamingContext context) {
+			info.AddValue("ID", this.ID);
+			info.AddValue("Name", this.Name);
+			info.AddValue("ParentDirectory", this.ParentDirectory);
+			info.AddValue("TargetDirectory", this.TargetDirectory);
+			info.AddValue("Extensions", this.Extensions);
+			info.AddValue("Filters", this.Filters);
+			info.AddValue("Instruction", this.Instruction);
+			info.AddValue("Subdirectories", this.SearchSubdirectories);
 		}
 
 		/* METHODS */
+		public string ToString() {
+			string buffer = "ID: " + this.ID + "\n" +
+							"Name: " + this.Name + "\n" +
+							"ParentDirectory: " + this.ParentDirectory + "\n" +
+							"TargetDirectory: " + this.TargetDirectory + "\n" +
+							"Extensions: " + string.Join(",", this.Extensions) + "\n" +
+							"Filters: " + string.Join(",", this.Filters) + "\n";
+
+			return buffer;
+		}
+
 		public void addExtension(string extension) {
 			this.Extensions.Add(extension);
 		}
 
 		public void addFilter(string filter) {
 			this.Filters.Add(filter);
+		}
+
+		public void Modify(int id,
+					string parentdir,
+					string targetdir,
+					List<string> extensions,
+					List<string> filters,
+					string name = "") {
+
+			this.ID = id;
+			this.ParentDirectory = parentdir;
+			this.TargetDirectory = targetdir;
+			this.Extensions = extensions;
+			this.Filters = filters;
+			this.Name = name;
 		}
 	}
 }
---
+++ fclib_Console/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fclib;
+
+namespace fclib_Console {
+	class Program {
+
+		private static string filepath = AppDomain.CurrentDomain.BaseDirectory + "rules.dat";
+
+		static void Main(string[] args) {
+			TestSave();
+			TestLoad();
+			Console.ReadLine();
+		}
+
+		static void TestLoad() {
+			Console.WriteLine(String.Format("Loading rules from {0} ...", filepath));
+			List<Rule> rulelist = UserData.LoadRules(filepath);
+			Console.WriteLine("Loaded rules:\n");
+			foreach (Rule rule in rulelist) {
+				Console.WriteLine(rule.ToString());
+			}
+		}
+
+		static void TestSave() {
+			string name = "Herp";
+			int id = 1;
+			string parentdir = "derpie";
+			string targetdir = "herbie";
+			List<string> extensions = new List<string>();
+			extensions.Add(".mkv");
+
+			List<string> filters = new List<string>();
+			filters.Add("anyad");
+
+			Console.WriteLine("Creating new Rule class instance");
+			Rule rule1 = new Rule(id, parentdir, targetdir, extensions, filters, name);
+			List<Rule> rulelist = new List<Rule>();
+			rulelist.Add(rule1);
+
+			Console.WriteLine(String.Format("Rule class created. Serializing to file {0} ...", filepath));
+			
+			UserData.SaveRules(rulelist, filepath);
+			
+			Console.ReadLine();
+		}
+	}
+}
